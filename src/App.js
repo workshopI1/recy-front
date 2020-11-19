9aa966782c920c273f123791c778ff5b8df6a0bd
Florian <florian.grember@gmail.com>
2020-11-19 18:04:19 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
+import React from "react";
 import './App.css';
 import Home from "./components/Home/Home";
 import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
-import React from "react";
 import {primary, secondary} from "./shared/colors";
 import {createMuiTheme} from "@material-ui/core";
 import {ThemeProvider} from "@material-ui/styles";
@@ -28,7 +28,6 @@ function App() {
   return (
   <ThemeProvider theme={theme}>
     <div className="App">
-        {/*<HomeAppBar/>*/}
         <Router>
             <Switch>
                 <Route exact path='/'>
