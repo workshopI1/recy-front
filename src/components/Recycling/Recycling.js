29ebf006b2f433d0ee697f57c7f734c403270dbc
Florian <florian.grember@gmail.com>
2020-11-19 08:17:09 +0100

    retry + homme button

--- src/components/Recycling/Recycling.js
+++ src/components/Recycling/Recycling.js
@@ -4,8 +4,12 @@ import HomeAppBar from "../Home/HomeAppBar";
 import DeleteIcon from '@material-ui/icons/Delete';
 import Card from "@material-ui/core/Card";
 import {makeStyles} from "@material-ui/styles";
-import Paper from "@material-ui/core/Paper";
+import ReplayIcon from '@material-ui/icons/Replay';
 import Divider from "@material-ui/core/Divider";
+import HomeIcon from '@material-ui/icons/Home';
+import Button from "@material-ui/core/Button";
+import {secondary} from "../../shared/colors";
+import {useHistory} from 'react-router-dom'
 
 
 const useStyles = makeStyles({
@@ -27,6 +31,15 @@ const useStyles = makeStyles({
     },
     green:{
       color:'#49ad49'
+    },
+    buttonDiv:{
+        display:'flex',
+        justifyContent:'space-around'
+    },
+    button:{
+        width:'45%',
+        backgroundColor:secondary,
+        color:'white'
     }
 })
 const Recycling = (props) => {
@@ -37,6 +50,11 @@ const Recycling = (props) => {
     const result = {
         name:'Bouteille de jus Innocent',
         recycl:'Bouteille'
+    }
+
+    const history = useHistory();
+    const handleClick = (route) => {
+        history.push(route)
     }
     const classes = useStyles();
     return (
@@ -57,9 +75,16 @@ const Recycling = (props) => {
                     <DeleteIcon className={classes.green} style={{fontSize:80}}/>
                     Container à verre : {result.recycl}
                 </div>
-                {/*<Typography>A recycler dans : {result.recycl}</Typography>*/}
             </Card>
             <Typography>Code trouvé : {code}</Typography>
+            <div className={classes.buttonDiv}>
+                <Button className={classes.button} onClick={() => handleClick('/scan')}>
+                    Scanner un autre produit <ReplayIcon/>
+                </Button>
+                <Button className={classes.button} onClick={() => handleClick('/home')}>
+                    <HomeIcon/> Retour à l'accueil
+                </Button>
+            </div>
         </>
     )
 }
